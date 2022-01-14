ca31f805c301335d566c90da9457cccdf3f215e3
Bryan Herrera <bcherrera14@gmail.com>
2022-01-14 07:19:37 -0800

    add feedback items and list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Header from './Components/Header';
+import FeedbackList from './Components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+	const [ feedback, setFeedback ] = useState(FeedbackData);
+
+	const deleteFeedback = (id) => {
+		if (window.confirm('Are you sure you want to delete?')) {
+			setFeedback(feedback.filter((item) => item.id !== id));
+		}
+	};
 	return (
 		<div>
 			<Header />
 			<div className="container">
-				<h1>Hello from App</h1>
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
 		</div>
 	);
